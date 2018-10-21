46628e78c35a91b9ccacfef68c2f0406221b733f
Blake Morrell <blknm4598@gmail.com>
2018-10-20 19:59:33 -0500

    Update movie.h

--- movie.h
+++ movie.h
@@ -1,22 +1,23 @@
 #ifndef MOVIE_H
 #define MOVIE_H
+#include <string>
 
 class Movie
 {
 	public:
 		Movie();
-		Movie(const std::string& name, const std::string& description, double score);
+		Movie(const std::string& name, const std::string& description, const std::string& score);
 		void setName(const std::string& name);
 		void setDescription(const std::string& description);
 		void setScore(double score);
 		std::string name() const;
 		std::string description() const;
-		double score() const;
+		std::string score() const;
 
 	private:
 		std::string name_;
 		std::string description_;
-		double score_;
+		std::string score_;
 };
 
 #endif //MOVIE_H
