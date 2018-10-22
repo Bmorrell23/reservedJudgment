91851d5005bb36b45a0005bd86e26e0b8e8cd80c
Asoelter <38193049+Asoelter@users.noreply.github.com>
2018-10-21 19:50:29 -0500

    Add files via upload

--- movie.h
+++ movie.h
@@ -11,24 +11,6 @@ class Movie : public Activity
 		Movie(const std::string& name, const std::string& description, 
 			  const std::string& score, const std::string& tags)
 		: Activity(name, description, score, tags){}
-		/*
-		Movie();
-		Movie(const std::string& name, const std::string& description, 
-			  const std::string& score, const std::string& tags);
-		void setName(const std::string& name);
-		void setDescription(const std::string& description);
-		void setScore(const std::string& score);
-		void setTag(const std::vector<std::string>& tags);
-		std::string name() const;
-		std::string description() const;
-		std::string score() const;
-		std::vector<std::string> tags() const;
-		std::string info() const;
-
-	private:*/
 };
-
-/*std::string substrBefore(const std::string& s, size_t index );
-std::string substrAfter(const std::string& s, size_t index);*/
 
 #endif //MOVIE_H
